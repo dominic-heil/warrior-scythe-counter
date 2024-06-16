fbcf3d286cca2139f86dec3f81b80cf04eb60cd9
dominic1608 <mail@d-heil.de>
2024-06-16 02:02:32 +0200

    v2

--- config_generator.js
+++ config_generator.js
@@ -1,36 +1,35 @@
 "use strict"
 
-
 const DefaultSettings = {
-    windowPos: [0, 0],
-    enabled: true,
-    onlySelf: false,
-    displayPriestBuffs: true,
-    displayMessage: false
-}   
+	windowPos: [0, 0],
+	enabled: true,
+	onlySelf: false,
+	displayPriestBuffs: true,
+	displayMessage: false
+}
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-    if (from_ver === undefined) {
-        return Object.assign(Object.assign({}, DefaultSettings), settings)
-    } else if (from_ver === null) {
-        return DefaultSettings
-    } else {
-        if (from_ver + 1 < to_ver) {
-            settings = MigrateSettings(from_ver, from_ver + 1, settings)
-            return MigrateSettings(from_ver + 1, to_ver, settings)
-        }
-        switch (to_ver) {
-            default:
-                let oldsettings = settings
+	if (from_ver === undefined) {
+		return Object.assign(Object.assign({}, DefaultSettings), settings)
+	} else if (from_ver === null) {
+		return DefaultSettings
+	} else {
+		if (from_ver + 1 < to_ver) {
+			settings = MigrateSettings(from_ver, from_ver + 1, settings)
+			return MigrateSettings(from_ver + 1, to_ver, settings)
+		}
+		switch (to_ver) {
+			default:
+				let oldsettings = settings
 
-                settings = Object.assign(DefaultSettings, {})
+				settings = Object.assign(DefaultSettings, {})
 
-                for (let option in oldsettings) {
-                    if (settings[option]) { settings[option] = oldsettings[option] }
-                }
-                break;
-        }
+				for (let option in oldsettings) {
+					if (settings[option]) { settings[option] = oldsettings[option] }
+				}
+				break;
+		}
 
-        return settings
-    }
+		return settings
+	}
 }
